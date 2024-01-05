3c1e01bf21cbb21347ae0cdfd2ea24190f9edefb
JunYoung <sksskdi7718@naver.com>
2024-01-05 09:59:40 +0900

    백준 정권지르기 5일차 시작

--- src/beakjoon/Main.java
+++ src/beakjoon/Main.java
@@ -3,17 +3,19 @@ package beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-	public static int min(int a, int b) {
-		return a < b ? a : b;
+	public static int max(int a, int b) {
+		return a > b ? a : b;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String[] st = new String[7];
-		
+		String[] st = new String[9];
+		int stSize = st.length;
+		int[] sevenDwarf = new int[7];
 		
 		
 	}
