4e1e3898ebc326f3f81f2f5015b100d6895a052b
JunYoung <sksskdi7718@naver.com>
2024-01-26 17:52:56 +0900

    박준영 : 헤헤

--- src/beakjoon/string/yosepus1158/Main.java
+++ src/beakjoon/string/yosepus1158/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
@@ -12,12 +13,27 @@ public class Main {
 		BufferedReader bf = 
 				new BufferedReader(new InputStreamReader(System.in));
 		String[] str = bf.readLine().split(" ");
+		Queue<Integer> qstr = new LinkedList<>();
+		Queue<Integer> result = new LinkedList<>();
 		int maxValue = Integer.parseInt(str[0]);
-		int fstPointer = Integer.parseInt(str[1]);
-		LinkedList<Integer> list = new LinkedList<>();
+		int Pointer = Integer.parseInt(str[1]);
 		
-		System.out.println(list.get(fstPointer));
+		for(int i=1;i<maxValue+1;i++) {
+			qstr.add(i);
+		}
 		
+		while(qstr.isEmpty() != true) {
+			
+			for(int k = 1; k<Pointer; k++) {
+				qstr.add(qstr.poll());
+			}
+			result.add(qstr.poll());
+		}
+		
+		String result1 = result.toString();
+		result1 = result1.replace("[", "<");
+		result1 = result1.replace("]", ">");
+		System.out.println(result1);
 	}
 
 }
